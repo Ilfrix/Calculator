b9604a588663e87e019ea4c77d262ad680cacef2
Vyacheslav <vya.polyanskiy@gmail.com>
2022-09-04 13:19:34 +0300

    refactoring code, v1.0.1

--- 2-10.py
+++ 2-10.py
@@ -1,5 +1,3 @@
-"Enter two decimal numbers separated by a space \
-the program will give you the answer in binary decimal form"
 def convert_value(n):   #function for converting values from decimal to binary, return string
     new_n = ''
     for i in range(len(n)):
@@ -8,23 +6,27 @@ def convert_value(n):   #function for converting values from decimal to binary, 
             tmp =(4 - len(tmp)) * '0' + tmp
         new_n += tmp
     return new_n
-print("Enter two decimal numbers separated by a space \
+
+print("Enter two decimal numbers separated by a space \n\
 the program will give you the answer in binary decimal form")
 first, second = map(str, input().split())
-new_first = ''
+
 first = convert_value(first)
 second = convert_value(second)
+
 #first number more or equel of length than second
 if (len(first) < len(second)):
     first, second = second, first
 second = (len(first) - len(second)) * '0' + second
 
+#output start values
 next_val = 0
 new_val = ''
 print("first value:\t", first)
 print("second value:\t", second)
-i = len(first) - 1
 
+#summarize values
+i = len(first) - 1
 while i >= 0 or next_val > 0:
     if i >= 0:
         tmp = int(first[i]) + int(second[i]) + next_val
@@ -34,6 +36,7 @@ while i >= 0 or next_val > 0:
     next_val = tmp // 2
     new_val += str(tmp % 2)
 
+#add zeroes if we have more digits, than after enter
 new_val = new_val[::-1]
 if (len(new_val) % 4 != 0):
     new_val = ((len(new_val) + 3) // 4 * 4 - len(new_val)) * '0' + new_val
@@ -55,8 +58,7 @@ if tetr != '':
     arr.append(tetr)
 
 
-
-#delete values more 9 from answer
+#delete values more 9(in decimal form) from answer
 i = len(arr) - 1
 next_val = 0
 while i >= 0:
@@ -75,9 +77,10 @@ answer = []
 if next_val > 0:
     answer.append(convert_value(str(next_val)))
 answer = answer + arr
+
+#output answer
 print()
 print("result is:\t", end="")
-#output answer
 for i in range(len(answer)):
     print(answer[i], end=" ")
 
